8fee7641260b000b6798eae04cd750b7413e15ed
Khushi Saxena <132602140+khushisaxena17@users.noreply.github.com>
2024-08-14 01:31:40 +0530

    Delete Exercise 7/EmployeeManagementSystem directory

--- Exercise 7/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EmployeeManagementSystem.repository;
-
-import com.example.EmployeeManagementSystem.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    // Derived query method to find departments by name
-    Department findByName(String name);
-}
